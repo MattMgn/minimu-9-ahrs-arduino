bbfa64d4e7315e6e506dd59922f4cb2df4a101d7
matt <matthieu.magnon@gadz.org>
2019-06-17 21:50:35 +0200

    minimu9ahrs: code cleaning

--- MinIMU9AHRS/MinIMU9AHRS.ino
+++ MinIMU9AHRS/MinIMU9AHRS.ino
@@ -38,15 +38,9 @@ with MinIMU-9-Arduino-AHRS. If not, see <http://www.gnu.org/licenses/>.
 // Positive pitch : nose up
 // Positive roll : right wing down
 // Positive yaw : clockwise
-int SENSOR_SIGN[9] = {1,1,1,-1,-1,-1,1,1,1}; //Correct directions x,y,z - gyro, accelerometer, magnetometer
-// Uncomment the below line to use this axis definition:
-   // X axis pointing forward
-   // Y axis pointing to the left
-   // and Z axis pointing up.
-// Positive pitch : nose down
-// Positive roll : right wing down
-// Positive yaw : counterclockwise
-//int SENSOR_SIGN[9] = {1,-1,-1,-1,1,1,1,-1,-1}; //Correct directions x,y,z - gyro, accelerometer, magnetometer
+int SENSOR_SIGN[9] = { 1,  1,  1,
+                      -1, -1, -1,
+                       1, 1,  1};
 
 // tested with Arduino Uno with ATmega328 and Arduino Duemilanove with ATMega168
 
@@ -54,34 +48,35 @@ int SENSOR_SIGN[9] = {1,1,1,-1,-1,-1,1,1,1}; //Correct directions x,y,z - gyro, 
 
 // accelerometer: 8 g sensitivity
 // 3.9 mg/digit; 1 g = 256
-#define GRAVITY 256  //this equivalent to 1G in the raw data coming from the accelerometer
+#define GRAVITY             256  //this equivalent to 1G in the raw data coming from the accelerometer
+#define NULL_VECTOR         {0, 0, 0}
 
-#define ToRad(x) ((x)*0.01745329252)  // *pi/180
-#define ToDeg(x) ((x)*57.2957795131)  // *180/pi
+#define ToRad(x)            ((x)*0.01745329252)  // *pi/180
+#define ToDeg(x)            ((x)*57.2957795131)  // *180/pi
 
 // gyro: 2000 dps full scale
 // 70 mdps/digit; 1 dps = 0.07
-#define Gyro_Gain_X 0.07 //X axis Gyro gain
-#define Gyro_Gain_Y 0.07 //Y axis Gyro gain
-#define Gyro_Gain_Z 0.07 //Z axis Gyro gain
-#define Gyro_Scaled_X(x) ((x)*ToRad(Gyro_Gain_X)) //Return the scaled ADC raw data of the gyro in radians for second
-#define Gyro_Scaled_Y(x) ((x)*ToRad(Gyro_Gain_Y)) //Return the scaled ADC raw data of the gyro in radians for second
-#define Gyro_Scaled_Z(x) ((x)*ToRad(Gyro_Gain_Z)) //Return the scaled ADC raw data of the gyro in radians for second
+#define Gyro_Gain_X         0.07 //X axis Gyro gain
+#define Gyro_Gain_Y         0.07 //Y axis Gyro gain
+#define Gyro_Gain_Z         0.07 //Z axis Gyro gain
+#define Gyro_Scaled_X(x)    ((x)*ToRad(Gyro_Gain_X))
+#define Gyro_Scaled_Y(x)    ((x)*ToRad(Gyro_Gain_Y))
+#define Gyro_Scaled_Z(x)    ((x)*ToRad(Gyro_Gain_Z))
 
 // LSM303/LIS3MDL magnetometer calibration constants; use the Calibrate example from
 // the Pololu LSM303 or LIS3MDL library to find the right values for your board
 
-#define M_X_MIN -1000
-#define M_Y_MIN -1000
-#define M_Z_MIN -1000
-#define M_X_MAX +1000
-#define M_Y_MAX +1000
-#define M_Z_MAX +1000
+#define M_X_MIN             -1000
+#define M_Y_MIN             -1000
+#define M_Z_MIN             -1000
+#define M_X_MAX             +1000
+#define M_Y_MAX             +1000
+#define M_Z_MAX             +1000
 
-#define Kp_ROLLPITCH 0.02
-#define Ki_ROLLPITCH 0.00002
-#define Kp_YAW 1.2
-#define Ki_YAW 0.00002
+#define Kp_ROLLPITCH        0.02
+#define Ki_ROLLPITCH        0.00002
+#define Kp_YAW              1.2
+#define Ki_YAW              0.00002
 
 /*For debugging purposes*/
 //OUTPUTMODE=1 will print the corrected data,
@@ -94,13 +89,13 @@ int SENSOR_SIGN[9] = {1,1,1,-1,-1,-1,1,1,1}; //Correct directions x,y,z - gyro, 
 
 #define STATUS_LED 13
 
-float G_Dt=0.02;    // Integration time (DCM algorithm)  We will run the integration loop at 50Hz if possible
+float G_Dt = 0.02;    // Integration time (DCM algorithm)  We will run the integration loop at 50Hz if possible
 
-long timer=0;   //general purpuse timer
+long timer = 0;   //general purpuse timer
 long timer_old;
-long timer24=0; //Second timer used to print values
+long timer24 = 0; //Second timer used to print values
 int AN[6]; //array that stores the gyro and accelerometer data
-int AN_OFFSET[6]={0,0,0,0,0,0}; //Array that stores the Offset of the sensors
+int AN_OFFSET[6] = {0, 0, 0, 0, 0, 0}; //Array that stores the Offset of the sensors
 
 int gyro_x;
 int gyro_y;
@@ -116,126 +111,118 @@ float c_magnetom_y;
 float c_magnetom_z;
 float MAG_Heading;
 
-float Accel_Vector[3]= {0,0,0}; //Store the acceleration in a vector
-float Gyro_Vector[3]= {0,0,0};//Store the gyros turn rate in a vector
-float Omega_Vector[3]= {0,0,0}; //Corrected Gyro_Vector data
-float Omega_P[3]= {0,0,0};//Omega Proportional correction
-float Omega_I[3]= {0,0,0};//Omega Integrator
-float Omega[3]= {0,0,0};
+float Accel_Vector[3] = NULL_VECTOR; //Store the acceleration in a vector
+float Gyro_Vector[3] = NULL_VECTOR;//Store the gyros turn rate in a vector
+float Omega_Vector[3] = NULL_VECTOR; //Corrected Gyro_Vector data
+float Omega_P[3] = NULL_VECTOR;//Omega Proportional correction
+float Omega_I[3] = NULL_VECTOR;//Omega Integrator
+float Omega[3] = NULL_VECTOR;
 
 // Euler angles
 float roll;
 float pitch;
 float yaw;
 
-float errorRollPitch[3]= {0,0,0};
-float errorYaw[3]= {0,0,0};
+float errorRollPitch[3] = NULL_VECTOR;
+float errorYaw[3] = NULL_VECTOR;
 
-unsigned int counter=0;
-byte gyro_sat=0;
+unsigned int counter = 0;
+byte gyro_sat = 0;
 
-float DCM_Matrix[3][3]= {
-  {
-    1,0,0  }
-  ,{
-    0,1,0  }
-  ,{
-    0,0,1  }
+float DCM_Matrix[3][3] = {
+    {1, 0, 0},
+    {0, 1, 0},
+    {0, 0, 1}
 };
-float Update_Matrix[3][3]={{0,1,2},{3,4,5},{6,7,8}}; //Gyros here
 
+float Update_Matrix[3][3] = {
+    {0, 1, 2},
+    {3, 4, 5},
+    {6, 7, 8}
+};
 
-float Temporary_Matrix[3][3]={
-  {
-    0,0,0  }
-  ,{
-    0,0,0  }
-  ,{
-    0,0,0  }
+float Temporary_Matrix[3][3] = {
+    {0, 0, 0},
+    {0, 0, 0},
+    {0, 0, 0}
 };
 
 void setup()
 {
-  Serial.begin(115200);
-  pinMode (STATUS_LED,OUTPUT);  // Status LED
-
-  I2C_Init();
-
-  Serial.println("Pololu MinIMU-9 + Arduino AHRS");
-
-  digitalWrite(STATUS_LED,LOW);
-  delay(1500);
-
-  Accel_Init();
-  Compass_Init();
-  Gyro_Init();
-
-  delay(20);
-
-  for(int i=0;i<32;i++)    // We take some readings...
-    {
-    Read_Gyro();
-    Read_Accel();
-    for(int y=0; y<6; y++)   // Cumulate values
-      AN_OFFSET[y] += AN[y];
+    Serial.begin(115200);
+    pinMode (STATUS_LED, OUTPUT);  // Status LED
+    
+    I2C_Init();
+    
+    Serial.println("Pololu MinIMU-9 + Arduino AHRS");
+    
+    digitalWrite(STATUS_LED, LOW);
+    delay(1500);
+    
+    Accel_Init();
+    Compass_Init();
+    Gyro_Init();
+    
     delay(20);
+    
+    for(int i = 0; i < 32; i++){
+        Read_Gyro();
+        Read_Accel();
+
+        for(int y = 0; y < 6; y++)   // Cumulate values
+            AN_OFFSET[y] += AN[y];
+        delay(20);
     }
+    
+    for(int y = 0; y < 6; y++)
+        AN_OFFSET[y] = AN_OFFSET[y]/32;
+    
+    AN_OFFSET[5] -= GRAVITY*SENSOR_SIGN[5];
 
-  for(int y=0; y<6; y++)
-    AN_OFFSET[y] = AN_OFFSET[y]/32;
+    //Serial.println("Offset:");
+    for(int y = 0; y < 6; y++)
+        Serial.println(AN_OFFSET[y]);
+    
+    delay(2000);
+     digitalWrite(STATUS_LED,HIGH);
 
-  AN_OFFSET[5]-=GRAVITY*SENSOR_SIGN[5];
-
-  //Serial.println("Offset:");
-  for(int y=0; y<6; y++)
-    Serial.println(AN_OFFSET[y]);
-
-  delay(2000);
-  digitalWrite(STATUS_LED,HIGH);
-
-  timer=millis();
-  delay(20);
-  counter=0;
+    timer=millis();
+    delay(20);
+    counter=0;
 }
 
-void loop() //Main Loop
-{
-  if((millis()-timer)>=20)  // Main loop runs at 50Hz
-  {
-    counter++;
-    timer_old = timer;
-    timer=millis();
-    if (timer>timer_old)
-    {
-      G_Dt = (timer-timer_old)/1000.0;    // Real time of loop run. We use this on the DCM algorithm (gyro integration time)
-      if (G_Dt > 0.2)
-        G_Dt = 0; // ignore integration times over 200 ms
+void loop() {
+    if((millis()-timer)>=20) {
+        counter++;
+        timer_old = timer;
+        timer = millis();
+        if (timer > timer_old) {
+            G_Dt = (timer - timer_old) / 1000.0;    // Real time of loop run. We use this on the DCM algorithm (gyro integration time)
+            if (G_Dt > 0.2)
+                G_Dt = 0.0; // ignore integration times over 200 ms
+        } else
+            G_Dt = 0.0;
+    
+    
+            // *** DCM algorithm
+            // Data adquisition
+            Read_Gyro();   // This read gyro data
+            Read_Accel();     // Read I2C accelerometer
+    
+            if (counter > 5)  // Read compass data at 10Hz... (5 loop runs)
+            {
+                counter = 0;
+                Read_Compass();    // Read I2C magnetometer
+                Compass_Heading(); // Calculate magnetic heading
+            }
+    
+            // Calculations...
+            Matrix_update();
+            Normalize();
+            Drift_correction();
+            Euler_angles();
+    
+        printdata();
     }
-    else
-      G_Dt = 0;
-
-
-
-    // *** DCM algorithm
-    // Data adquisition
-    Read_Gyro();   // This read gyro data
-    Read_Accel();     // Read I2C accelerometer
-
-    if (counter > 5)  // Read compass data at 10Hz... (5 loop runs)
-    {
-      counter=0;
-      Read_Compass();    // Read I2C magnetometer
-      Compass_Heading(); // Calculate magnetic heading
-    }
-
-    // Calculations...
-    Matrix_update();
-    Normalize();
-    Drift_correction();
-    Euler_angles();
-    // ***
-
-    printdata();
-  }
 
 }
